9a6dd082cc511dccb8aff4161263c0041b6191c5
vee4n <alvian.dvno@gmail.com>
2024-09-24 18:13:37 +0700

    .env.ts modified

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,8 +1,8 @@
 export const NODE_ENV = process.env.NODE_ENV;
-export const PASSWORD = "nickolasgay";
+export const PASSWORD = process.env.PASSWORD;
 
 // export const PASSWORD = process.env.PASSWORD;
 export const SECRET = process.env.SECRET;
 // export const DATABASE_URL= "prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZGZmOTFkNzYtMDgxYi00NGE2LWJjZWEtNDA3NjEzY2NjNWVhIiwidGVuYW50X2lkIjoiYWNkNmY1NjExMTdiOTQxZDk0ZGJiYmIwODA1ZTlmN2MyODljMjY3YWYwMzU2MTI4ZDVmMThmNTgxN2FhOTlmMyIsImludGVybmFsX3NlY3JldCI6IjM5NDNjYjBkLTQ2MGYtNGVkNC1iZDcxLWMxODI5MTk5MzE0MSJ9.GKgKIuBAfIIQMk8yvZjxpqQVcSUd44YVfSGwM3dnuxY";
 // export const DIRECT_URL="<YOUR_DATABASE_CONNECTION_STRING>";   
-export const DATABASE_URL="postgres://default:3qPp6KEDdfIh@ep-lingering-union-a1f2uw3y-pooler.ap-southeast-1.aws.neon.tech/verceldb?sslmode=require"
+export const DATABASE_URL= process.env.DATABASE_URL
