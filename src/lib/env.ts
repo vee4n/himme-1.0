8563f1b669f3141c59cee69bcb139cf7949ea69e
vee4n <alvian.dvno@gmail.com>
2024-09-24 20:02:57 +0700

    gitignore env and remove database url from env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,4 @@
 export const NODE_ENV = process.env.NODE_ENV;
 export const PASSWORD = process.env.PASSWORD;
 export const SECRET = process.env.SECRET;
-export const DATABASE_URL= process.env.DATABASE_URL
+// export const DATABASE_URL= process.env.DATABASE_URL
