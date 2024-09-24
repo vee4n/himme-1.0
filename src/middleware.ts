8e8eee2a6d501d7942097f5722552c00a3bc11a5
vee4n <alvian.dvno@gmail.com>
2024-09-24 17:42:44 +0700

    remove check in from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { SECRET } from "~/lib/env";
 import { verify } from "~/lib/jwt";
 
-const PROTECTED_ROUTES = [/\/dashboard/, /\/check-in\/.+/];
+const PROTECTED_ROUTES = [/\/dashboard/];
 
 export default async function middleware(req: NextRequest) {
   const { cookies, nextUrl } = req;
@@ -14,7 +14,6 @@ export default async function middleware(req: NextRequest) {
 
   if (PROTECTED_ROUTES.filter((route) => route.test(currentUrl)).length > 0) {
     if (!token) {
-      console.log("Redirecting to home: no token");
       return NextResponse.redirect(redirectUrl);
     }
 
